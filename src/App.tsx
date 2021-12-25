3c5c67aea4f885372713e29ed63c2c4264e0bf63
skd6150 <skd6150@gmail.com>
2021-12-25 21:12:30 +0900

    Apply navigation to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {MainScreen} from './Screens';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={MainScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
