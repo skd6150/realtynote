849a1fc2ed1313afed03b9402ee97ea7f8448882
skd6150 <skd6150@gmail.com>
2021-12-26 19:18:11 +0900

    Add note screen and apply to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import configureStore from './Redux/Reducer';
-import {MainScreen} from './Screens';
+import {MainScreen, NoteScreen} from './Screens';
+import {StackParamList} from './Interfaces';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<StackParamList>();
 const {store, persistor} = configureStore();
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Note" component={NoteScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       </PersistGate>
