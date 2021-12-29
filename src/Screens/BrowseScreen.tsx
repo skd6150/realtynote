bc34601655701fffd8fc38ae5d5340341a6953e0
skd6150 <skd6150@gmail.com>
2021-12-29 23:22:39 +0900

    Remove if statement from browse screen

--- src/Screens/BrowseScreen.tsx
+++ src/Screens/BrowseScreen.tsx
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useLayoutEffect} from 'react';
 import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import {
   BrowseScreenNavigationProps,
   BrowseScreenRouteProps,
 } from '../Interfaces';
 import {useStringfyRentalType} from '../Hooks';
+import {HeaderRight} from '../Components';
 
 type BrowseScreenProps = {
   route: BrowseScreenRouteProps;
@@ -28,111 +29,114 @@ const Row = ({label, content}: RowProps) => {
 const Devider = () => <View style={styles.devider} />;
 
 const BrowseScreen = ({route, navigation}: BrowseScreenProps) => {
-  const [item, setItem] = useState(route.params);
-  if (item) {
-    return (
-      <ScrollView>
+  const [note, setNote] = useState(route.params);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderRight
+          label="수정"
+          callback={() => {
+            navigation.navigate('Post', note);
+          }}
+        />
+      ),
+    });
+  });
+  return (
+    <ScrollView>
+      <View
+        style={{
+          alignSelf: 'stretch',
+          height: 240,
+          backgroundColor: '#C4C4C4',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Text>사진</Text>
+      </View>
+      <View style={styles.cardWrapper}>
+        <View style={styles.row}>
+          <Text style={[styles.label, styles.text16]}>임대조건</Text>
+          <View style={styles.content}>
+            <Text style={styles.text20}>{useStringfyRentalType(note)}</Text>
+            {note.managementFee !== 0 && (
+              <Text style={styles.text12}>
+                (관리비 {note.managementFee / 10000}만원)
+              </Text>
+            )}
+          </View>
+        </View>
+        <Row label="주소" content={note.address} />
         <View
           style={{
             alignSelf: 'stretch',
-            height: 240,
+            height: 180,
+            margin: 20,
             backgroundColor: '#C4C4C4',
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <Text>사진</Text>
+          <Text>지도</Text>
         </View>
-        <View style={styles.cardWrapper}>
-          <View style={styles.row}>
-            <Text style={[styles.label, styles.text16]}>임대조건</Text>
-            <View style={styles.content}>
-              <Text style={styles.text20}>{useStringfyRentalType(item)}</Text>
-              {item.managementFee !== 0 && (
-                <Text style={styles.text12}>
-                  (관리비 {item.managementFee / 10000}만원)
-                </Text>
-              )}
-            </View>
-          </View>
-          <Row label="주소" content={item.address} />
-          <View
-            style={{
-              alignSelf: 'stretch',
-              height: 180,
-              margin: 20,
-              backgroundColor: '#C4C4C4',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Text>지도</Text>
-          </View>
-          <Text style={[styles.sectionLabel, styles.text18]}>구조/면적</Text>
+        <Text style={[styles.sectionLabel, styles.text18]}>구조/면적</Text>
+        <Row
+          label="층/건물층수"
+          content={`${note.floor.evaluations[note.floor.idx]}/${
+            note.roofFloor.evaluations[note.roofFloor.idx]
+          }`}
+        />
+        <Row
+          label="구조"
+          content={note.roomStructure.evaluations[note.roomStructure.idx]}
+        />
+        <Row label="전용면적" content={`${note.size}평`} />
+        {note.sturctureEvaluationFactors.map((factor, index) => (
           <Row
-            label="층/건물층수"
-            content={`${item.floor.evaluations[item.floor.idx]}/${
-              item.roofFloor.evaluations[item.roofFloor.idx]
-            }`}
+            key={`sturctureEvaluationFactor-${index}`}
+            label={factor.name}
+            content={factor.evaluations[factor.idx]}
           />
+        ))}
+        <Devider />
+        <Text style={[styles.sectionLabel, styles.text18]}>옵션/환경</Text>
+        <Row
+          label="옵션"
+          content={
+            note.options
+              .filter(note => note.available)
+              .map(note => note.name)
+              .join(', ') || '없음'
+          }
+        />
+        {note.environmentEvaluationFactors.map((factor, index) => (
           <Row
-            label="구조"
-            content={item.roomStructure.evaluations[item.roomStructure.idx]}
+            key={`environmentEvaluationFactors-${index}`}
+            label={factor.name}
+            content={factor.evaluations[factor.idx]}
           />
-          <Row label="전용면적" content={`${item.size}평`} />
-          {item.sturctureEvaluationFactors.map((factor, index) => (
-            <Row
-              key={`sturctureEvaluationFactor-${index}`}
-              label={factor.name}
-              content={factor.evaluations[factor.idx]}
-            />
-          ))}
-          <Devider />
-          <Text style={[styles.sectionLabel, styles.text18]}>옵션/환경</Text>
-          <Row
-            label="옵션"
-            content={
-              item.options
-                .filter(item => item.available)
-                .map(item => item.name)
-                .join(', ') || '없음'
-            }
-          />
-          {item.environmentEvaluationFactors.map((factor, index) => (
-            <Row
-              key={`environmentEvaluationFactors-${index}`}
-              label={factor.name}
-              content={factor.evaluations[factor.idx]}
-            />
-          ))}
-          <Devider />
-          <Text style={[styles.sectionLabel, styles.text18]}>메모</Text>
-          <View
-            style={{
-              alignSelf: 'stretch',
-              minHeight: 100,
-              margin: 20,
-              padding: 10,
-              backgroundColor: '#FBFBFB',
-              borderRadius: 10,
-              borderWidth: 1,
-              borderColor: '#DDE1E5',
-            }}>
-            <Text>{item.memo}</Text>
-          </View>
-          <Devider />
-          <Text style={[styles.sectionLabel, styles.text18]}>공인중개사</Text>
-          <Row label="상호명" content={item.realtorName} />
-          <Row label="연락처" content={item.realtorTel} />
+        ))}
+        <Devider />
+        <Text style={[styles.sectionLabel, styles.text18]}>메모</Text>
+        <View
+          style={{
+            alignSelf: 'stretch',
+            minHeight: 100,
+            margin: 20,
+            padding: 10,
+            backgroundColor: '#FBFBFB',
+            borderRadius: 10,
+            borderWidth: 1,
+            borderColor: '#DDE1E5',
+          }}>
+          <Text>{note.memo}</Text>
         </View>
-      </ScrollView>
-    );
-  } else {
-    console.error('cannot find note');
-    return (
-      <View>
-        <Text>error</Text>
+        <Devider />
+        <Text style={[styles.sectionLabel, styles.text18]}>공인중개사</Text>
+        <Row label="상호명" content={note.realtorName} />
+        <Row label="연락처" content={note.realtorTel} />
       </View>
-    );
-  }
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
